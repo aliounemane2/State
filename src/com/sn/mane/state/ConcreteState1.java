d309046178aa155e2e91db06a8e86803436195fe
Alioune MANE <aliounemane@finetechgroupe.com>
2022-02-06 18:23:31 +0000

    Design Pattern State

---
+++ src/com/sn/mane/state/ConcreteState1.java
@@ -0,0 +1,19 @@
+package com.sn.mane.state;
+
+
+/**
+ * ConcreteState1 implements a behavior associated with a state of the Context.
+ *
+ */
+public class ConcreteState1 implements State {
+
+    private boolean handleInvoked = false;
+
+    public void handle() {
+        this.handleInvoked = true;
+    }
+
+    protected boolean isHandleInvoked() {
+        return handleInvoked;
+    }
+}
---
+++ src/com/sn/mane/state/State.java
@@ -0,0 +1,12 @@
+package com.sn.mane.state;
+
+/**
+ * The interface for encapsulating the behavior associated with a particular
+ * state of the Context.
+ *
+ */
+public interface State {
+
+    void handle();
+
+}
